5b45be6ecbeab03bc58f26adadb2ed7e51ed63ae
Artem <40538552+surge603@users.noreply.github.com>
2022-02-20 20:25:45 +0300

    Задание 18

--- tsk18/Program.cs
+++ tsk18/Program.cs
@@ -1,2 +1,24 @@
 ﻿// Задача 18: Проверить истинность утверждения
 // ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+bool[] x = { true, true, false, false };
+bool[] y = { true, false, true, false };
+
+Console.Clear();
+Console.WriteLine("Проверяем истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y:");
+bool result = true;
+for (int i = 0; i < 4; i++)
+{
+    Console.Write($"X:{x[i]}; Y:{y[i]} - ");
+    if (!(x[i] || y[i]) == !x[i] && !y[i])
+        Console.WriteLine("верно.");
+    else
+    {
+        Console.WriteLine("НЕВЕРНО!");
+        result = false;
+    }
+}
+if (result)
+    Console.WriteLine("Утверждение верно.");
+else
+    Console.WriteLine("УТВЕРЖДЕНИЕ НЕВЕРНО!");
