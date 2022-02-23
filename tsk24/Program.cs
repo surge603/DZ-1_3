6d4154cfe485aee09aedeaaebb581b96753c9405
Artem <40538552+surge603@users.noreply.github.com>
2022-02-23 08:34:47 +0300

    Задание 24

---
+++ tsk24/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 24: Найти кубы чисел от 1 до N
+
+Console.Clear();
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i=1;i<=n;i++){
+    Console.WriteLine($"N = {i}, N^3 = {Math.Pow(i,3)}");
+
+}
