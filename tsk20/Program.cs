2303c32bc17b3148ea47672368453e45e968aa26
Artem <40538552+surge603@users.noreply.github.com>
2022-02-23 07:32:00 +0300

    Задание 20

---
+++ tsk20/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 20: Задать номер четверти, показать диапазоны для возможных координат
+
+Console.Clear();
+Console.Write("Задайте номер четверти системы координат (1-4): ");
+int n = int.Parse(Console.ReadLine());
+if (n < 0 || n > 4)
+{
+    Console.WriteLine("Номер вне заданных параметров!");
+    return;
+}
+    Console.WriteLine("Диапазоны координат:");
+if (n==1)
+{
+    Console.WriteLine("x>0, y>0");
+}
+else if (n==2)
+{
+    Console.WriteLine("x<0, y>0");
+}
+else if (n==3)
+{
+    Console.WriteLine("x<0, y<0");
+}
+else 
+{
+    Console.WriteLine("x>0, y>0");
+}
